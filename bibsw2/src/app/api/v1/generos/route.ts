d09da080dfb68d93d02700380fc0b93096916779
AndreRonqui <160617515+AndreRonqui@users.noreply.github.com>
2025-05-05 11:38:56 +0000

    aula 05/05/2025

--- bibsw2/src/app/api/v1/generos/route.ts
+++ bibsw2/src/app/api/v1/generos/route.ts
@@ -1,8 +1,16 @@
 import{ NextResponse } from "next/server";
 
 import dados from '@/db.json';
+import {request} from "http";
 
 export async function GET() {
     //return NextResponde.json({"mensagem" : "Generos});
+    return NextResponse.json(dados);
+}
+
+
+export async function POST(request : Request){
+    const data = await request.json();
+    dados.push(data);
     return NextResponse.json(dados);
 }
