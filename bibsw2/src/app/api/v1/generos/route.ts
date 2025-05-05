3835f6752d58be4bef5ce1e238e2d7b97fc71ff3
AndreRonqui <160617515+AndreRonqui@users.noreply.github.com>
2025-05-05 10:47:10 +0000

    rest client

--- bibsw2/src/app/api/v1/generos/route.ts
+++ bibsw2/src/app/api/v1/generos/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import{ NextResponse } from "next/server";
+
+import dados from '@/db.json';
 
 export async function GET() {
-    return NextResponse.json({"mensagem" : "Generos"});
+    //return NextResponde.json({"mensagem" : "Generos});
+    return NextResponse.json(dados);
 }
-
